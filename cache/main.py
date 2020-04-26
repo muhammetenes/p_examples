71bbe138d2af809cd042f3a85366c8f1ec17ab21
muhammetenes <muhammet.enesak@gmail.com>
2020-04-26 22:59:06 +0300

    updated main.py

--- cache/main.py
+++ cache/main.py
@@ -1,6 +1,5 @@
-import pandas as pd
-from pandas import DataFrame
 import sys
+from datetime import date, timedelta
 
 
 class Example:
@@ -8,29 +7,45 @@ class Example:
     max_cache_size: int = 5000
 
     def __init__(self):
-        self.data = pd.read_csv("cache/owid-covid-data.csv", index_col=["date"])
+        self.data = open("cache/owid-covid-data.csv", "r").readlines()
 
     @staticmethod
-    def get_key(country, start_date, end_date) -> str:
-        return f"{country}-{start_date}-{end_date}"
+    def get_key(country, date) -> str:
+        return f"{country}-{date}"
 
     def remove_oldest(self) -> None:
         oldest_key: str = list(self.cache.keys())[0]
         self.cache.pop(oldest_key)
 
-    def set_cache(self, key: str, data: DataFrame) -> None:
+    def set_cache(self, key, data):
         if self.size > self.max_cache_size:
             self.remove_oldest()
-        self.cache[key] = data.to_dict()["total_cases"]
+        self.cache[key] = data
 
-    def get_data(self, country: str, start_date: str, end_date: str) -> DataFrame:
-        key = self.get_key(country, start_date, end_date)
-        if self.cache.get(key):
-            return self.cache[key]
-        else:
-            data = self.data[self.data["location"] == country][start_date: end_date]
-            self.set_cache(key, data)
-            return data.to_dict()["total_cases"]
+    def get_data(self, country, date):
+        for item in self.data:
+            split_data = item.split(",")
+            if split_data[1] == country and split_data[2] == str(date):
+                return split_data[4]  # new_case
+        return
+
+    def collect_data(self, country: str, start_date: str, end_date: str):
+        s_year, s_month, s_day = start_date.split("-")
+        e_year, e_month, e_day = end_date.split("-")
+        s_date = date(int(s_year), int(s_month), int(s_day))
+        e_date = date(int(e_year), int(e_month), int(e_day))
+        delta = e_date - s_date
+        result = {}
+        for i in range(delta.days + 1):
+            d = s_date + timedelta(days=i)
+            key = self.get_key(country, d)
+            if self.cache.get(key):
+                result[key] = self.cache[key]
+            else:
+                data = self.get_data(country, d)
+                result[key] = data
+                self.set_cache(key, data)
+        return result
 
     @property
     def size(self) -> int:
@@ -46,6 +61,6 @@ if __name__ == "__main__":
         start_date = input()
         print("End date")
         end_date = input()
-        print(c.get_data(country, start_date, end_date))
+        print(c.collect_data(country, start_date, end_date))
 
 
